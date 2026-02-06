b8371174b16544157bc211740f0faacb0b93b4ea
daoman <956448+daoman@users.noreply.github.com>
2026-02-06 20:49:27 +0300

    Bump version from 1.0.0 to 1.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = readme_file.read_text(encoding="utf-8") if readme_file.exists
 
 setup(
     name="upd-builder",
-    version="1.0.0",
+    version="1.0.2",
     author="Georgy",
     author_email="givi.zirabuch@gmail.com",
     description="Генератор валидного УПД (Универсального Передаточного Документа) в формате XML файла в соответствии со спецификацей XSD v5.03",
